b45330c2f37bf5a536bc658b8df70655e519771f
Ragde007 <edgdelcast91@gmail.com>
2022-03-09 22:26:22 -0500

    cambio en el allowhost

--- LifeHack_web/settings.py
+++ LifeHack_web/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
+ALLOWED_HOSTS = ['*', '.herokuapp.com']
 
 
 # Application definition
@@ -153,3 +153,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
--- staticfiles/base/js/miJs.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
- console.log('hola puto mundo, desde miJs')
-});
-
-//jquerys 
-//=============== parallax
-$(document).ready(function(){
-    $('.parallax').parallax();
-  }); 
