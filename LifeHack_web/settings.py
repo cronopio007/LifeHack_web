02a24b67a962a4c8302fd1b1838f3727eea3aeb7
Ragde007 <edgdelcast91@gmail.com>
2022-03-10 17:33:33 -0500

    cambio de rutas static

--- LifeHack_web/settings.py
+++ LifeHack_web/settings.py
@@ -126,15 +126,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+# os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT= BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 
 
 #tambien se define aca el path del los static, para q use el los archivos de la carpeta
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    )
+#(os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [BASE_DIR /'static' ,]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
