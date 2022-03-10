938a6b6b4337416d20f550b89b4fb238da446846
Ragde007 <edgdelcast91@gmail.com>
2022-03-09 20:07:03 -0500

    cambio del settings

--- LifeHack_web/settings.py
+++ LifeHack_web/settings.py
@@ -19,7 +19,7 @@ import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Activate Django-Heroku.
 # django_heroku.settings(locals())
