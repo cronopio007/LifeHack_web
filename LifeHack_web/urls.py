a5a85581bf66fc124ab1436e4587360c7a95d02a
Ragde007 <edgdelcast91@gmail.com>
2022-03-10 23:44:14 -0500

    correccion de la version d django

--- LifeHack_web/urls.py
+++ LifeHack_web/urls.py
@@ -12,9 +12,15 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+    lass-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
