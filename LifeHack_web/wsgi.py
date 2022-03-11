d9c410a8c31c8799c36fb2ef5e9a42fa777c126e
Ragde007 <edgdelcast91@gmail.com>
2022-03-11 11:26:24 -0500

    StaticFilesHandler

--- LifeHack_web/wsgi.py
+++ LifeHack_web/wsgi.py
@@ -11,8 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 #from whitenoise.django import DjangoWhiteNoise  
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LifeHack_web.settings')
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
+#application = get_wsgi_application()
 #application = DjangoWhiteNoise(application)
